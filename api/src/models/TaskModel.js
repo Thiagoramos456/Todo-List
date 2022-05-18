88ba86ceabfb69d9be3301ed441552da0e3cc165
Thiagoramos456 <thiagovirtual10@gmail.com>
2022-05-18 18:14:24 -0300

    feat: cria model p/ create:tasks

--- api/src/models/TaskModel.js
+++ api/src/models/TaskModel.js
@@ -5,6 +5,12 @@ async function getAll() {
   return tasks ? tasks : [];
 }
 
+async function create() {
+  const [task] = await connection.execute('INSERT INTO tasks (title, description, status) VALUES (?, ?, ?)', ['Task 1', 'Task 1 description']);
+  return task;
+}
+
 module.exports = {
   getAll,
+  create,
 };
