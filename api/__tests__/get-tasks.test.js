77d395aa234abcb5a2e133d3e0d793c5dad80624
Thiagoramos456 <thiagovirtual10@gmail.com>
2022-05-18 17:50:31 -0300

    tests: quando nao ha tasks em getAll tasks

--- api/__tests__/get-tasks.test.js
+++ api/__tests__/get-tasks.test.js
@@ -14,26 +14,43 @@ const { expect } = chai;
 describe('GET /tasks', () => {
   let res;
 
-  before(async() => {
-    sinon.stub(connection, 'execute').resolves([tasksMock]);
-    res = await chai.request(app).get('/tasks');
+  describe('Quando há tasks', () => {
+    before(async () => {
+      sinon.stub(connection, 'execute').resolves([tasksMock]);
+      res = await chai.request(app).get('/tasks');
+    });
+
+    it('O status deve ser 200 OK', async () => {
+      expect(res).to.have.status(200);
+      expect(res.body).to.be.deep.equal(tasksMock);
+    });
+
+    it('O retorno deve ser um array', async () => {
+      expect(res.body).to.be.an('array');
+      expect(res.body).to.have.length(3);
+    });
+
+    it('O retorno deve ser um array de tasks', async () => {
+      expect(res.body).to.be.deep.equal(tasksMock);
+    });
   });
-  
-  it('O status deve ser 200 OK', async () => {
-    expect(res).to.have.status(200);
 
-    expect(res.body).to.be.deep.equal(tasksMock);
+  describe('Quando não há tasks', () => {
+    before(async () => {
+      sinon.stub(connection, 'execute').resolves([]);
+      res = await chai.request(app).get('/tasks');
+    });
+
+    it('O status deve ser 404 NOT FOUND', async () => {
+      expect(res).to.have.status(404);
+    });
+
+    it('O retorno deve ser uma string', async () => {
+      expect(res.body).to.be.an('string');
+    });
+
+    it('O retorno deve ser uma mensagem de erro', async () => {
+      expect(res.body).to.be.equal('Nenhuma task encontrada');
+    });
   });
-
-  it('O retorno deve ser um array', async () => {
-    expect(res.body).to.be.an('array');
-    expect(res.body).to.have.length(3);
-  });
-
-  it('O retorno deve ser um array de tasks', async () => {
-    expect(res.body).to.be.deep.equal(tasksMock);
-  });
-
-  
-
 });
--- api/src/controllers/TaskController.js
+++ api/src/controllers/TaskController.js
@@ -2,6 +2,7 @@ const TaskService = require('../services/TaskService');
 
 async function getAll(_req, res) {
   const tasks = await TaskService.getAll();
+
   return res.status(200).json(tasks);
 }
 
